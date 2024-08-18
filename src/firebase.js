f6c43ab83a3e919dc830fdb6340cc0525617e35b
sakshampurohitt <127320787+sakshampurohitt@users.noreply.github.com>
2024-08-18 22:15:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDCm-YvtbdCPhm9cGQ9BPSqQx4KQuOM-8k",
-  authDomain: "wealthify-7c945.firebaseapp.com",
-  projectId: "wealthify-7c945",
-  storageBucket: "wealthify-7c945.appspot.com",
-  messagingSenderId: "976570455437",
-  appId: "1:976570455437:web:c2d730436b2ac6cb8419c4",
-  measurementId: "G-MM7D31BG49"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
